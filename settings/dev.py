2cdaee7033740d67d4c574beca73e6ff5beb049f
Vaibhav <Vaibhav>
2024-12-06 16:46:37 +0000

    working nginx, nodejs prod, django debug=False mode

--- settings/dev.py
+++ settings/dev.py
@@ -5,7 +5,7 @@ import warnings
 # Database
 # https://docs.djangoproject.com/en/1.10.2/ref/settings/#databases
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["bpc.opencv.org"]
 
@@ -34,44 +34,13 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
 
-# DJANGO-SPAGHETTI-AND-MEATBALLS SETTINGS
-INSTALLED_APPS += [  # noqa: ignore=F405
-    "django_spaghetti",
-    "autofixture",
-    "debug_toolbar",
-    "django_extensions",
-    "silk",
-]
-
-SPAGHETTI_SAUCE = {
-    "apps": [
-        "auth",
-        "accounts",
-        "analytics",
-        "base",
-        "challenges",
-        "hosts",
-        "jobs",
-        "participants",
-        "web",
-    ],
-    "show_fields": True,
-}
-
-CACHES = {
-    "default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"},
-    "throttling": {"BACKEND": "django.core.cache.backends.locmem.LocMemCache"},
-}
-
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = "/api/static/"
 MEDIA_URL = "/api/media/"
-
-MIDDLEWARE += [  # noqa: ignore=F405
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
-    "silk.middleware.SilkyMiddleware",
-]
-
-SILKY_PYTHON_PROFILER = True
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Ensure this line is present
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  # Optional: Define MEDIA_ROOT if you handle media files
+print(f"BASE_DIR: {BASE_DIR}")
+print(f"STATIC_ROOT: {STATIC_ROOT}")
 
 # Prevents Datetime warning by showing errors
 warnings.filterwarnings(
@@ -80,3 +49,36 @@ warnings.filterwarnings(
     RuntimeWarning,
     r"django\.db\.models\.fields",
 )
+
+# =----------------------------------------------------------------
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = (
+    "http://bpc.opencv.org",
+    "https://bpc.opencv.org",
+)
+# E-Mail Settings
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+ACCOUNT_DEFAULT_HTTP_PROTOCOL = "http"
+
+
+# Creates issue with normal work
+CACHES["default"]["LOCATION"] = os.environ.get("MEMCACHED_LOCATION", None)
+print('CACHES["default"]["LOCATION"]', CACHES["default"]["LOCATION"])
+
+
+# Use local storage
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+# STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.CompressedManifestStaticFilesStorage'
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
+
+# Hide API Docs on production environment
+REST_FRAMEWORK_DOCS = {"HIDE_DOCS": True}
+
+# # # https://docs.djangoproject.com/en/1.10/ref/settings/#secure-proxy-ssl-header
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
